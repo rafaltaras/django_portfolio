4f54e4e4e56ef2d1c60db756a6def39ffd5302a4
rafaltaras <paulo.dorosz@gmail.com>
2023-03-11 09:10:15 +0100

    2nd task

--- portfolio/infos/views.py
+++ portfolio/infos/views.py
@@ -3,22 +3,10 @@ from django.http import HttpResponse
 from django.template import Context, loader
 
 def mainpage(request):
-    return render(
-    	    request=request,
-    	    template_name="infos/mainpage.html",
-    	    # context=c
-	)
+    return render(request, "infos/base.html", {'navbar': 'base'})
 
 def me(request):
-    return render(
-    	    request=request,
-    	    template_name="infos/me.html",
-    	    # context=c
-	)
+    return render(request, "infos/me.html", {'navbar': 'me'})
 
 def contact(request):
-    return render(
-    	    request=request,
-    	    template_name="infos/contact.html",
-    	    # context=c
-	)
+    return render(request, "infos/contact.html", {'navbar': 'contact'})
